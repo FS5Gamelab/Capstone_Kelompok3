a273192eb16ee80a8ae4bc994163a9f18f2245cd
yusrilshina <yusrilshina04@gmail.com>
2024-05-24 21:46:08 +0700

    midleware admin

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $role = Auth()->user()->role;
+            if ($role == 'admin') {
+                return view('admin.dashboard');
+            } else if ($role == 'user') {
+                return view('dashboard');
+            } else {
+                return redirect()->back();
+            }
+        }
+    }
+
+    public function post(){
+        return 'iki post';
+    }
+}
