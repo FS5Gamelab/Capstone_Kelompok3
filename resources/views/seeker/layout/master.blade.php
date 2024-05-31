0d702f1d9815461b3aa9f17e1637c3c4efaa81d6
nadila firda <nadilafirda33@gmail.com>
2024-05-31 11:39:26 +0800

    dashboard user

---
+++ resources/views/seeker/layout/master.blade.php
@@ -0,0 +1,11 @@
+@include('seeker.layout.header')
+@include('seeker.layout.navbar')
+@include('seeker.layout.sidebar')
+@include('seeker.layout.content')
+@include('seeker.layout.footer')
+
+
+
+
+
+
